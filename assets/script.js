0e66919016322c2c68e58bafaf2d84e67feb6784
Bri <bhilliker@gmail.com>
2021-06-27 20:58:08 -0500

    added submit score function and end quiz function

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // GLOBAL VARIABLES
-// get all the html elements we need as js
+// quiz questions and answers
 var $question = document.getElementById('question');
 var $choice0 = document.getElementById('choice-0')
 var $choice1 = document.getElementById('choice-1')
@@ -9,24 +9,29 @@ var $choice3 = document.getElementById('choice-3')
 var $startButton = document.getElementById('start');
 var $highScoreForm = document.getElementById('high-score-form');
 var $questionContainer = document.getElementById("questionContainer");
-var quizEl = document.getElementById("question")
-var optionsEl= document.getElementsByClassName("choice"); // i think returns an array of elements
-var questionContainerEl = document.getElementById('questionContainer')
-var go = document.getElementById('start');
+var optionsEl= document.getElementsByClassName("choice"); // returns an array of elements
+var $answerbtn = document.getElementById("answer-btn")
+var $submit = document.getElementById("submit")
 
 let currentQuestionIndex 
+let ansCorrect = 0
+let finalScore = parseInt(ansCorrect) + (timeLeft)
 
-go.addEventListener("click", startQuiz);
-go.onclick=startTimer;
+$startButton.addEventListener("click", startQuiz);
+$startButton.onclick=startTimer;
+
+var timeLeft = 75;
 
 function startTimer(){
-    var timeLeft = 75;
+    timeLeft = 75;
     var elem = document.getElementById('timer');
     var timerId = setInterval(countdown, 1000);
-    go.classList.add(`hide`);
+    $startButton.classList.add(`hide`);
     function countdown() {
-        if (timeLeft == 0) {
+        if (timeLeft <= 0) {
             clearTimeout(timerId);
+            endQuiz();
+
         } else {
             elem.innerHTML = "Time: " + timeLeft ;
             timeLeft--;
@@ -35,19 +40,49 @@ function startTimer(){
 }    
 
 function displayNextQuestion(){
-    console.log('display next question function invoked. reset happend and now next question should appear')
-    console.log('question being passed to show question')
+    // console.log('display next question function invoked. reset happend and now next question should appear')
+    // console.log('question being passed to show question')
     var i = currentQuestionIndex
-    console.log(i)
-    console.log(questions[i])
-    showQuestion(questions[i])
+    if (i < questions.length){
+        showQuestion(questions[i])
+    }else{
+        endQuiz()
+    }
 }        
+
 function endQuiz(){
+    $answerbtn.classList.add('hide')
+    $highScoreForm.classList.remove('hide')
+    $startButton.classList.add('hide')
+    $questionContainer.classList.add('hide')
+    $submit.classList.remove('hide')
+    alert('we made it to the finish line')
+
+    alert( 'Your final score is ' + finalScore);
+    var urscore = document.getElementById('urscore');
+    urscore.innerHTML = 'Your Final Score is ' + finalScore ;
+    saveScores()
+    displayHighScores()
+    
+}
+
+function displayHighScores(){
+    document.getElementById("submit").onclick=function(){
+        var winner = parseInt("urscore");
+        location.href = "./high-scores.html";
+    // put the object into local storage
+    // localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    }
+}
+var saveScores = function() {
+    localStorage.setItem("finalScore", JSON.stringify(finalScore));
 }
 
 function startQuiz(){
     console.log('start quiz was started')
-    go.classList.add('hide');
+    $startButton.classList.add('hide');
+    $answerbtn.classList.remove('hide')
     currentQuestionIndex = 0;
     displayNextQuestion();
     console.log(question);
@@ -70,73 +105,63 @@ function showQuestion(question){
 function handleAnswerSubmission(e) {
     e.preventDefault();
     console.log(e.target.dataset.correct);
+    let button = e.target
     if (e.target.dataset.correct === e.target.textContent) {
+        ansCorrect = ansCorrect + 1
         //correct
-
+        // add a point to score
+        // button.classList.add(".correct")
         alert('you did it!')
     } else {
-        // incorrect
+        // wrong
+        // button.classList.add(".wrong")
+        timeLeft = timeLeft - 10;
         alert('wrong')
     }
     // move onto next question
-
-}
-function selectAnswer(){
-    console.log("you clicked me")
-    
-    button.addEventListener ('click', $choice0.textContent )
-        if (answers[i] === correctAnswer){
-            // correct
-            // add point to score
-        }
-        else{
-            // wrong
-        }
-    button.addEventListener ('click', $choice1.textContent )
-    button.addEventListener ('click', $choice2.textContent )
-    button.addEventListener ('click', $choice3.textContent )
+    displayNextQuestion(currentQuestionIndex++)
 }
 
 var questions = [
 { question: "This is question 1!" , 
-answers: ["hamsterWheel", "hamster", "choice3", "choice4"],
+answers: ["hamsterWheel", "hamster", "bulldozer", "love"],
 correctAnswer: 'hamster'  
 },
 { question: "This is question 2!" , 
-answers: ["choice1", "cheese", "choice3", "choice4"],
+answers: ["chicken", "cheese", "cherries"],
 correctAnswer: "cheese"
 },
 { question: "This is question 3!" , 
-answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 3
+answers: ["monopoly", "trouble", "jenga", "chutes and ladders"],
+correctAnswer: "jenga"
 },
 { question: "This is question 4!" , 
 answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 1  
+correctAnswer: "choice1"  
 },
 { question: "This is question 5!" , 
 answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 3
+correctAnswer: "choice3"
 },
 { question: "This is question 6!" , 
 answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 2
+correctAnswer: "choice2"
 },
 { question: "This is question 7!" , 
 answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 2  
+correctAnswer: "choice2"  
 },
 { question: "This is question 8!" , 
 answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 0
+correctAnswer: "choice0"
 },
 { question: "This is question 9!" , 
 answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 1
+correctAnswer: "choice2"
 },
 { question: "This is question 10!" , 
 answers: ["choice1", "choice2", "choice3", "choice4"],
-correctAnswer: 0  
+correctAnswer: "choice0"  
 },]
 
 $choice0.addEventListener('click', handleAnswerSubmission);
